d641b65a3a1ba44e5e15018cba8e837c92285918
urssanjaysingh <sanjaysingh26112000@gmail.com>
2023-10-29 11:32:16 +0530

    Ready To Deploy

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/product', productRoutes)
 
 app.get('/', (req, res) => {
-    res.send('<div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f8f9fa;">' +
-        '<h1 style="color: #007bff; font-family: Arial, sans-serif; text-align: center; background-color: #ffffff; padding: 20px; border-radius: 8px;">Welcome to E-Commerce App</h1>' +
+    res.send('<div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f8f9fa; margin: 0; padding: 0;">' +
+        '<h1 style="color: #007bff; font-family: Arial, sans-serif; text-align: center; background-color: #ffffff; padding: 20px; border-radius: 8px; margin: 0;">Welcome to E-Commerce App</h1>' +
         '</div>');
 });
 
