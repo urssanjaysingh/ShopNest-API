db3cfa6eaa2ea418db6fd9b1ef5c107bdbd424b4
urssanjaysingh <sanjaysingh26112000@gmail.com>
2023-10-29 11:18:47 +0530

    Ready To Deploy

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/product', productRoutes)
 
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to E-Commerce App</h1>")
-})
+app.get('/welcome', (req, res) => {
+    res.send('<h1 style="color: #007bff; font-family: Arial, sans-serif; text-align: center;">Welcome to E-Commerce App</h1>');
+});
 
 const PORT = process.env.PORT || 3500;
 
